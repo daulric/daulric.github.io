088b52d80e534aa6fcccea48d1c6e6361b21a367
Ulric <ulricaird1@gmail.com>
2024-06-05 11:48:13 -0400

    updated front page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,7 @@ import LinkCard from "@/components/LinkCard"
 export default function AboutMe(){
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    <code>About Ulric</code> 
-                </p>
-            </div>
+            
 
             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
                 <Image
@@ -27,7 +23,15 @@ export default function AboutMe(){
                     <center> My Name is Ulric</center><br/>
                     Student of the TA Marryshow Community College<br/>
                 </p>
+
             </div>
+
+            <br/>
+            
+            <p className={`mb-3 text-2xl font-semibold`} >
+                Socials<br/>
+            </p>
+
 
             <br />
 
@@ -52,6 +56,38 @@ export default function AboutMe(){
                     info="Send a Personal Message"
                 />
             </div>
+
+            <br/><br/>
+
+            <div id="projects" className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+                <p className={`mb-3 text-2xl font-semibold`} >
+                    <center> Projects</center><br/>
+                </p>
+            </div>
+
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+                <LinkCard
+                    link="https://github.com/daulric/exon"
+                    text="Exon"
+                    info="Framework for Game Development and more..."
+                    newTab={true}
+                />
+
+                <LinkCard
+                    link="https://www.roblox.com/groups/12852480"
+                    text="Games"
+                    info="This is a group with the list of game I made..."
+                />
+
+                <LinkCard 
+                    link="https://github.com/daulric/DatabaseServiceApi"
+                    text="Database Service API"
+                    info="Created to connect and handle Firebase Realtime Database"
+                />
+
+            </div>
+
         </main>
     )
 }
