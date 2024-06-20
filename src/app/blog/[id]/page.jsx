b21c8a571238aeda1cdbd1d4c61ded4617fe73f7
Ulric <ulricaird1@gmail.com>
2024-06-20 19:26:32 -0400

    added stuff

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default async function BlogIndividual( props ) {
+
+    
+    return (
+
+        <React.Fragment>
+            <h1> It works  </h1>
+
+            <p> idk </p>
+
+        </React.Fragment>
+
+    )
+}
--- src/app/blog/success/route.js
+++ src/app/blog/success/route.js
@@ -1,19 +1,56 @@
 //import { redirect } from "next/navigation"
 import { NextResponse } from "next/server"
 
+import { initializeApp } from "firebase/app"
+import { ref, get, set, getDatabase } from "firebase/database"
+import { redirect } from "next/navigation"
+
 export async function GET(req) {
     let { searchParams } = new URL(req.url)
     let title = searchParams.get('title')
     let content = searchParams.get('content')
+
+    const app = initializeApp({
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+        measurementId: process.env.measurementId,
+    })
+
+    let db = getDatabase(app)
 
     try {
         if (title === null || content === null || title === "" || content === "") {
             throw new Error("There is no title or content!")
         }
 
+        let currentId = 0;
+
+        let currentIdRef = ref(db, "/blogs/currentId")
+
+        await get(currentIdRef).then((snapshot) => {
+            let tempId = snapshot.val();
+            if (typeof(tempId) === "number") {
+                currentId = tempId;
+            }
+        })
+
+        currentId += 1;
+
+        await set(ref(db, `/blogs/${currentId}`), {
+            blog_id: currentId,
+            title: title,
+            content: content,
+        }).then(() => {
+            set(currentIdRef, currentId)
+        })
+
+        return await NextResponse.redirect(new URL(`/blog/${currentId}`, req.url))
     } catch(err) {
         return NextResponse.json({err}, {status: 500})
     }
 
-    return NextResponse.json({blog_id}, {status: 200})
 }
