df24b3400d029e2d061f45bfd5905511883b148f
Ulric <ulricaird1@gmail.com>
2024-06-21 16:42:08 -0400

    blog landing page in the making

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { initializeApp } from "firebase/app";
 import { ref, get, getDatabase } from "firebase/database"
 
+import LinkCard from "@/components/LinkCard";
+
 import "./style.css"
 
 export default async function BlogIndividual( props ) {
@@ -30,48 +32,63 @@ export default async function BlogIndividual( props ) {
         data = {
             title: "Error with Blog",
             content: "Blog not found!"
-        } 
-    } else {
-        console.log(response.val())
-    
-        let compare_id = Number(data.blog_id);
-        let compare_id2 = Number(blog_id)
-
-        if (compare_id !== compare_id2 ) {
-            throw new Error("Invalid Blog ID!")
         }
-
-        console.log("Final Values", data)
+    } else if (Number(data.blog_id) !== Number(blog_id)) {
+        data = {
+            title: `Blog ID not matched in database!`,
+            content: `${data.blog_id} is not the same blog as ${blog_id} from the database!`
+        }
     }
-
     
     return (
-        <main id="mainclass" className="flex min-h-screen flex-col items-center justify-between p-20">
+        <React.Fragment>
 
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-                    src="/logo.png"
-                    alt="Logo"
-                    width={180}
-                    height={37}
-                    priority
+            <main id="mainclass" className="flex min-h-screen flex-col items-center justify-between p-20">
+
+                <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+                    <Image
+                        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                        src="/logo.png"
+                        alt="Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+
+                </div>
+
+                <br/><br/>
+
+                <div id="email-b" className="flex min-h-screen flex-col items-center justify-between p-1">
+                    <p className={`mb-3 text-2xl font-semibold`} >
+                        <center id="title-msg" >{data.title}</center>
+
+                        <br/><br/>
+                        <p id="content-msg">{data.content} </p>
+                    </p>
+
+                    
+                </div>
+
+                <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+                <LinkCard
+                    link="/blog"
+                    text="Return to Blogs"
+                    info="Returns to the blog page to view more blogs!"
+                />
+
+                <LinkCard
+                    link="/home"
+                    text="Return Home"
+                    info="Returns to the home page"
                 />
 
             </div>
+                
+            </main>
 
-            <br/><br/>
-
-            <div id="email-b" className="flex min-h-screen flex-col items-center justify-between p-1">
-                <p className={`mb-3 text-2xl font-semibold`} >
-                    <center id="title-msg" >{data.title}</center>
-
-                    <br/><br/>
-                    <p id="content-msg">{data.content} </p>
-                </p>
-            </div>
-            
-        </main>
+        </React.Fragment>
 
     )
 }
