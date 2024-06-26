b4176f3d6872233bb6aec8de483e8688c3d12c68
Ulric <ulricaird1@gmail.com>
2024-06-26 17:06:01 -0400

    api update

--- src/app/blog/route.js
+++ src/app/blog/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     let blog_ref = ref(db, `/blogs/data`)
-    let data = await get(blog_ref)
-    return NextResponse.json(data.val())
+    let data = await get(blog_ref).then(response => response.val())
+    console.log(data)
+    return NextResponse.json(data)
 }
