280f5794813baa21feda7d111c9ddc5b58e85824
Ulric <ulricaird1@gmail.com>
2024-07-16 15:31:45 -0400

    added a special blog feature

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -26,46 +26,103 @@ async function getData () {
         data.push(temp_data)
     })
 
-    data.sort((a, b) => (b.blog_id - a.blog_id)) // Sorts in descending order to get the recent blogs posted!
-    return data;
+    let blog_with_nums = data.filter(blog => (typeof blog.blog_id === "number"))
+    let blog_with_str = data.filter(blog => (typeof blog.blog_id === "string"))
+
+    blog_with_nums.sort((a, b) => (b.blog_id - a.blog_id))
+    // Sorts in descending order to get the recent blogs posted!
+    return {
+        items: blog_with_nums,
+        special_items: blog_with_str,
+    }
 
 }
 
 async function handleBlogs() {
     noStore()
 
-    const items = await getData()
+    const { items } = await getData()
 
     if ( items === null) {
-        return  <React.Fragment />;
+        return null;
     }
 
     if ( items.length === 0) {
-        return <React.Fragment />;
+        return null;
     }
 
     return (
         <React.Fragment>
-            { 
-                items.map((item) => {
-                    if (typeof item === "number") return null;
-                    if (typeof item.timeCreated === "undefined") return null;
+            <div id="normal" />
+            <br/>
+            <div className="blog-heading">
+                <h3>Recent Anonymous Blogs</h3>
+            </div>
 
-                    return (
-                        <BlogCard
-                            key={item.blog_id}
-                            title={item.title}
-                            info={item.content}
-                            link={`/blog/${item.blog_id}`}
-                            date={`${item.timeCreated.yr}/${item.timeCreated.month}/${item.timeCreated.date}`}
-                        />
-                    )
-                })
-            }
+            <div className="blog-container">
+                { 
+                    items.map((item) => {
+                        if (typeof item === "number") return null;
+                        if (typeof item.timeCreated === "undefined") return null;
+
+                        return (
+                            <BlogCard
+                                key={item.blog_id}
+                                title={item.title}
+                                info={item.content}
+                                link={`/blog/${item.blog_id}`}
+                                date={`${item.timeCreated.yr}/${item.timeCreated.month}/${item.timeCreated.date}`}
+                            />
+                        )
+                    })
+                }
+            </div>
 
         </React.Fragment>
-
     )
+}
+
+async function handleSpecialBlogs() {
+    noStore()
+    const { special_items } = await getData()
+
+    if ( special_items === null) {
+        return  null;
+    }
+
+    if ( special_items.length === 0) {
+        return null;
+    }
+
+    return (
+        <React.Fragment >
+            <div id="special" />
+            <br/>
+            <div className="blog-heading">
+                <h3>Special Anonymous Blogs</h3>
+            </div>
+
+            <div className="blog-container">
+                { 
+                    special_items.map((item) => {
+                        if (typeof item === "number") return null;
+                        if (typeof item.timeCreated === "undefined") return null;
+
+                        return (
+                            <BlogCard
+                                key={item.blog_id}
+                                title={item.title}
+                                info={item.content}
+                                link={`/blog/${item.blog_id}`}
+                                date={`${item.timeCreated.yr}/${item.timeCreated.month}/${item.timeCreated.date}`}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </React.Fragment>
+    )
+
 }
 
 export default async function BlogLandingPage() {
@@ -85,7 +142,7 @@ export default async function BlogLandingPage() {
                 </div>
 
                 <div className="blog-heading">
-                    <h3>Recent Anonymous Blogs</h3>
+                    <h3>Anonymous Blogs</h3>
                     <br/><br/>
                     <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                         <LinkCard text="Create Blog" info="Create an Anonymous Blog" link="/blog/create" />
@@ -94,10 +151,11 @@ export default async function BlogLandingPage() {
                     
                 </div>
 
-                <div className="blog-container">
-                    
-                        { handleBlogs() }
-                </div>
+                <br/>
+
+                { handleSpecialBlogs() }
+                <br/><br/>
+                { handleBlogs() }
             </section>
         </React.Fragment>
     )
